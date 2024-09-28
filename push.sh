34a4f33539245a0d2b2e6c423791f5a6e6f60cd9
Dat Nguyen <datnguyen1215@gmail.com>
2024-09-28 07:51:52 -0700

    Update dotfiles

--- push.sh
+++ push.sh
@@ -2,7 +2,6 @@ mkdir -p ./.config/
 cp -r ~/.config/nvim/ .config/
 cp -r ~/.config/kitty/ .config/
 cp -r ~/.config/i3/ .config/
-cp -r ~/.config/i3status/ .config/
 cp -r ~/.config/gtk-3.0/ .config/
 cp -r ~/.tmux.conf ./
 
