04f0160735d2cac8aba0f2999c706b957789aaf9
Dat Nguyen <datnguyen1215@gmail.com>
2024-09-28 07:47:32 -0700

    update arch packages.

---
+++ arch/install/packages.sh
@@ -0,0 +1,4 @@
+pacman -Syu github-cli obsidian kitty less lazygit ripgrep networkmanager nm-connection-editor network-manager-applet lightdm i3 i3status neovim tmux gcc dmenu firefox xorg-xinput bluez blueman openssh git
+pacman -S bluez bluez-utils
+pacman -S zip unzip
+pacman -S lxappearance-gtk3
