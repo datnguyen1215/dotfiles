f39ff67771e57a5a5396f85176b5c2a075f6ba51
Dat Nguyen <datnguyen1215@gmail.com>
2024-09-29 22:14:12 -0700

    Update dotfiles

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,13 +2,9 @@
 
 # Terminate already running bar instances
 killall -q polybar
-pkill -f ~/.config/polybar/scripts/fullscreen-handler.sh
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
 polybar main &
-
-# Launch the fullscreen handler
-~/.config/polybar/scripts/fullscreen-handler.sh &
