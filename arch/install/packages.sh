16292a75169a8fcb530bf366e05fd0ad3a6c8f9c
Dat Nguyen <datnguyen1215@gmail.com>
2024-09-29 18:21:54 -0700

    install packages automatically.

--- arch/install/packages.sh
+++ arch/install/packages.sh
@@ -1,43 +1,39 @@
-sudo pacman -Syu git github-cli
-sudo pacman -S openssh kitty less
-sudo pacman -S networkmanager nm-connection-editor network-manager-applet
-sudo pacman -S lightdm i3 i3status
-sudo pacman -S xorg-xinput
-sudo pacman -S dmenu firefox
+sudo pacman -Syyu git github-cli
+sudo pacman -Sy gcc i3status kitty less xorg-xinput firefox openssh
 
 sudo # TODO: Remove this if no longer need to use obsidian
-sudo pacman -Syu obsidian
+sudo pacman -Sy obsidian
 
 sudo # Support bluetooth
-sudo pacman -S bluez blueman bluez-utils
+sudo pacman -Sy bluez blueman bluez-utils
 
 sudo # Zip/unzip files
-sudo pacman -S zip unzip tar
+sudo pacman -Sy zip unzip tar
 
 sudo # TODO: Not sure if this is needed, should check
-sudo pacman -S lxappearance-gtk3
+sudo pacman -Sy lxappearance-gtk3
 
 sudo # Support background & transparency
-sudo pacman -S feh picom
+sudo pacman -Sy feh picom
 
-sudo pacman -S thunar
+sudo pacman -Sy thunar
 
 sudo # Image viewer
-sudo pacman -S mupdf
+sudo pacman -Sy mupdf
 
 sudo # Support neovim
 sudo # TODO: Also install nvm and nodejs
-sudo pacman -S neovim tmux lazygit ripgrep
+sudo pacman -Sy neovim tmux lazygit ripgrep
 
 sudo # installing sound controls
-sudo pacman -S pipewire pipewire-alsa pipewire-audio pipewire-pulse wireplumber pavucontrol
+sudo pacman -Sy pipewire pipewire-alsa pipewire-audio pipewire-pulse wireplumber pavucontrol
 
 # required for polybar's codes
-sudo pacman -S polybar xdotool wmctrl playerctl
+sudo pacman -Sy polybar xdotool wmctrl playerctl
 
 # allow controlling screen's brightness
-sudo pacman -S brightnessctl
+sudo pacman -Sy brightnessctl
 
 # install timeshift backup tool and create a backup
-sudo pacman -S timeshift
+sudo pacman -Sy timeshift
 sudo timeshift --create
