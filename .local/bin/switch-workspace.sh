057520f3156ee21d086eb2d58d400c9d978e8d19
Dat Nguyen <datnguyen1215@gmail.com>
2024-09-30 08:08:25 -0700

    Update dotfiles

--- .local/bin/switch-workspace.sh
+++ .local/bin/switch-workspace.sh
@@ -9,12 +9,17 @@ TARGET_WORKSPACE_2=$(($TARGET_WORKSPACE_1 + 10))
 FOCUSED_WORKSPACE=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | tr -d '"')
 
 # Switch to target workspace
-i3-msg -t run_command "workspace --no-auto-back-and-forth $TARGET_WORKSPACE_1"
-i3-msg -t run_command "workspace --no-auto-back-and-forth $TARGET_WORKSPACE_2"
-
+#i3-msg -t run_command "workspace $TARGET_WORKSPACE_1; workspace $TARGET_WORKSPACE_2"
+#i3-msg -t run_command "workspace --no-auto-back-and-forth $TARGET_WORKSPACE_2"
+#
+#sleep 0.1
 # if focused_workspace > 10, switch to target 2
 if [ $FOCUSED_WORKSPACE -gt 10 ]; then
-	i3-msg -t run_command "workspace --no-auto-back-and-forth $TARGET_WORKSPACE_2"
+	i3-msg -t run_command "workspace $TARGET_WORKSPACE_1; workspace $TARGET_WORKSPACE_2"
+  sleep 0.2s
+	i3-msg -t run_command "focus output HDMI-0"
 else
-	i3-msg -t run_command "workspace --no-auto-back-and-forth $TARGET_WORKSPACE_1"
+	i3-msg -t run_command "workspace $TARGET_WORKSPACE_2; workspace $TARGET_WORKSPACE_1"
+  sleep 0.2s
+	i3-msg -t run_command "focus output DP-0"
 fi
