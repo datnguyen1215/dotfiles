5bbcd5a1e220412fd2942874d7e139e3470ea1a7
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-01 01:29:27 -0700

    Update dotfiles

---
+++ .local/bin/webhooks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PROJECT_PATH=~/personal/projects/webhooks/
+
+tmux split-pane -v -t :.1
+
+tmux send-keys -t :.1 "cd $PROJECT_PATH" C-m
+tmux send-keys -t :.1 'npm run dev' C-m
+
+tmux send-keys -t :.2 'ngrok http 9500 --url living-ready-glider.ngrok-free.app' C-m
