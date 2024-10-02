96283c3c3f9aafc42d74a6e41ee4a4141c0f8e0a
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-02 04:50:20 -0700

    stop copying i3 and timeshift configuration.

--- copy-configs.sh
+++ copy-configs.sh
@@ -6,7 +6,6 @@ rm -rf .config/*
 mkdir -p .config/
 cp -r ~/.config/nvim/ .config/
 cp -r ~/.config/kitty/ .config/
-cp -r ~/.config/i3/ .config/
 cp -r ~/.config/gtk-3.0/ .config/
 cp -r ~/.config/polybar/ .config/
 cp -r ~/.config/picom/ .config/
@@ -22,9 +21,6 @@ cp -r ~/.local/share/themes/ .local/share/
 cp -r ~/.local/bin/ .local/
 
 cp -r ~/backgrounds/ ./
-
-mkdir -p etc/
-cp -r /etc/timeshift etc/
 
 # commit changes
 git add .
