2097cef673c319a4649d04d160f60dbec923f64e
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-10 10:37:55 -0700

    update nvim.

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -1,4 +1,36 @@
-return {
+local M = {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" },
+  opts = {
+    filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+  },
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
+      pattern = opts.filetype_exclude,
+      callback = function()
+        require("ufo").detach()
+      end,
+    })
+
+    vim.opt.foldlevelstart = 99
+    -- Option 2: nvim lsp as LSP client
+    -- Tell the server the capability of foldingRange,
+    -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+    for _, ls in ipairs(language_servers) do
+      require("lspconfig")[ls].setup({
+        capabilities = capabilities,
+        -- you can add other fields for setting up lsp server in this table
+      })
+    end
+    require("ufo").setup(opts)
+  end,
 }
+
+return M
