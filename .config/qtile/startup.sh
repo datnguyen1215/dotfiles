b8e678a68ca0750c18f98978ec3283621f5aa311
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-26 07:31:04 -0700

    disable sound card.

--- .config/qtile/startup.sh
+++ .config/qtile/startup.sh
@@ -4,6 +4,9 @@ xset s off          # Disable screen saver
 xset -dpms          # Disable DPMS (Energy Star) features
 xset s noblank      # Disable screen blanking
 
+# disable the Built-In Audio Sound
+pactl set-card-profile alsa_card.pci-0000_00_1b.0 off
+
 feh --bg-scale ~/backgrounds/joker.jpg
 
 pkill picom
