d5319848970e25d101f0a767d0408ee75f03a525
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-27 18:12:15 -0700

    recursively fold descendants using zC.

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -3,6 +3,46 @@ vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 
+local fold_descendants = function()
+  local ts_utils = require("nvim-treesitter.ts_utils")
+  local current_node = ts_utils.get_node_at_cursor()
+  if not current_node then
+    print("No current node found")
+    return
+  end
+
+  -- Expand to the nearest enclosing foldable node
+  while current_node and not current_node:named() do
+    current_node = current_node:parent()
+  end
+
+  if not current_node then
+    print("No foldable node found")
+    return
+  end
+
+  -- Collect and fold all child nodes
+  local nodes = {}
+  local function collect_nodes(node)
+    table.insert(nodes, node)
+    for child in node:iter_children() do
+      collect_nodes(child)
+    end
+  end
+
+  collect_nodes(current_node)
+
+  -- Fold nodes in reverse order (deepest first)
+  for i = #nodes, 1, -1 do
+    local node = nodes[i]
+    local start_line = node:start() + 1
+    local end_line = node:end_() + 1
+    if start_line < end_line then
+      vim.cmd(start_line .. "," .. end_line .. "fold")
+    end
+  end
+end
+
 local handler = function(virtText, lnum, endLnum, width, truncate)
   local newVirtText = {}
   local lastText = vim.fn.getline(endLnum):gsub("^%s*(.-)%s*$", "%1")
@@ -51,11 +91,17 @@ return {
     },
 
     init = function()
+      vim.keymap.set("n", "zC", fold_descendants, { noremap = true, silent = true })
+
       vim.keymap.set("n", "zR", function()
         require("ufo").openAllFolds()
       end)
       vim.keymap.set("n", "zM", function()
         require("ufo").closeAllFolds()
+      end)
+      vim.keymap.set("v", "zc", function()
+        -- fold the selected text
+        vim.cmd("normal! zf")
       end)
     end,
   },
