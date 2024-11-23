a27b34c3942d4557f366a384761bd486816ff8f2
Dat Nguyen <datnguyen1215@gmail.com>
2024-11-23 04:36:27 -0800

    update nvim config.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,4 +10,5 @@ require("config.keymaps")
 require("config.code-copy")
 require("buffermind")
 vim.g.autoformat = false
+vim.opt.conceallevel = 0
 vim.api.nvim_command("highlight UfoLineCountGroup guifg=#000000 guibg=#efb700 gui=bold")
