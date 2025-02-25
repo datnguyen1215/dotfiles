d1508f1c13ec8ebef9fde5b448be638776602290
Dat Nguyen <datnguyen1215@gmail.com>
2025-02-25 15:31:01 -0800

    update webhook commands.

--- .local/bin/webhooks.sh
+++ .local/bin/webhooks.sh
@@ -7,4 +7,4 @@ tmux split-pane -v -t :.1
 tmux send-keys -t :.1 "cd $PROJECT_PATH" C-m
 tmux send-keys -t :.1 'npm run dev' C-m
 
-tmux send-keys -t :.2 'ngrok http 9500 --url living-ready-glider.ngrok-free.app' C-m
+tmux send-keys -t :.2 'ngrok http 127.0.0.1:9500 --url living-ready-glider.ngrok-free.app' C-m
