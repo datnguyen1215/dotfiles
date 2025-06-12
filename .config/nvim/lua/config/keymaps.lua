c4eeeeaad381b3ccf96454eb6ada55a24386f5f8
Dat Nguyen <datnguyen1215@gmail.com>
2025-06-11 19:15:43 -0700

    update new nvim setup.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,60 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Split horizontally
 vim.api.nvim_set_keymap("n", "<leader>s", ":vs<CR>", { silent = true })
-
--- Split vertically
 vim.api.nvim_set_keymap("n", "<leader>S", ":split<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ac", ":AvanteClear<CR>", { silent = true })
-
-vim.api.nvim_set_keymap("n", "<leader>oil", ":Octo issue list<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>oic", ":Octo issue create<CR>", { silent = true })
-
-vim.api.nvim_set_keymap("n", "<leader>opl", ":Octo pr list<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>opc", ":Octo pr create<CR>", { silent = true })
 
 vim.api.nvim_set_keymap("n", "y", '"+y', { noremap = true })
 vim.api.nvim_set_keymap("v", "y", '"+y', { noremap = true })
-
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>hx",
-  ':lua require("harpoon.mark").add_file()<CR>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope harpoon marks<CR>", { noremap = true, silent = true })
--- quick navigation
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>he",
-  ":lua require('harpoon.ui').toggle_quick_menu()<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>1",
-  ':lua require("harpoon.ui").nav_file(1)<CR>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>2",
-  ':lua require("harpoon.ui").nav_file(2)<CR>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>3",
-  ':lua require("harpoon.ui").nav_file(3)<CR>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>4",
-  ':lua require("harpoon.ui").nav_file(4)<CR>',
-  { noremap = true, silent = true }
-)
 
 vim.keymap.set("v", "<leader>d", function()
   -- Yank the visual selection to register
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,73 +3,11 @@
 -- Add any additional options here
 vim.g.mapleader = ";"
 
+-- disable keymaps that conflict with LazyVim
 vim.opt.clipboard = ""
-vim.o.sessionoptions = vim.o.sessionoptions:gsub("folds", "")
-vim.o.cursorline = false
 
--- create new files in the directory of the current buffer
---
-local Path = require("plenary.path")
+-- disable autoformatting on save
+vim.g.autoformat = false
 
--- Function to create a new file in the directory of the current buffer
-local function create_new_file_in_buffer_dir()
-  -- Get the current buffer's file path
-  local current_file = vim.fn.expand("%:p")
-  local current_dir = Path:new(current_file):parent().filename
-
-  -- Prompt for the new file name
-  local new_file_name = vim.fn.input("New file name: ", "", "file")
-
-  if new_file_name == "" then
-    return
-  end
-
-  -- Generate the full path for the new file
-  local new_file_path = Path:new(current_dir, new_file_name).filename
-
-  -- Open the new file
-  vim.cmd("e " .. new_file_path)
-end
-
--- Bind the function to a keymap, for example, <leader>nf
-vim.keymap.set("n", "<leader>nf", create_new_file_in_buffer_dir, { noremap = true, silent = true })
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = function()
-    if vim.fn.isdirectory(vim.fn.expand("%")) == 1 then
-      vim.cmd("cd " .. vim.fn.expand("%")) -- Change directory to the opened directory
-      vim.cmd("enew") -- Open a new empty buffer
-      vim.cmd("bdelete #") -- Delete the previous buffer
-    end
-  end,
-})
-
--- Function to get the last segment of the current Neovim working directory
-local function get_last_dir_name()
-  local cwd = vim.fn.getcwd()
-  return vim.fn.fnamemodify(cwd, ":t")
-end
-
--- Function to rename the tmux session
-local function rename_tmux_session()
-  local session_name = get_last_dir_name()
-  -- Check if running inside tmux
-  if os.getenv("TMUX") then
-    -- Use os.execute to run tmux command.
-    os.execute("tmux rename-window " .. session_name)
-  end
-end
-
--- Set up an autocommand for VimEnter to rename the session when Neovim starts
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    rename_tmux_session()
-  end,
-})
-
-vim.api.nvim_create_autocmd("VimLeave", {
-  callback = function()
-    os.execute("tmux rename-window " .. "bash")
-  end,
-})
+-- Set the default colors for the Ufo line count
+vim.api.nvim_command("highlight UfoLineCountGroup guifg=#000000 guibg=#efb700 gui=bold")
